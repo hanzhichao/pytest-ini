831c4c462835deda4dfbfc1affc2e741e325d596
superhin <superhin@126.com>
2022-04-25 21:11:58 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,24 @@
 # -*- coding: utf-8 -*-
 
 """The setup script."""
-
+import os
 from setuptools import setup, find_packages
 
+this_directory = os.path.abspath(os.path.dirname(__file__))
 setup_requirements = ['pytest-runner', ]
+
+
+def read_file(filename):
+    with open(os.path.join(this_directory, filename), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
 
 setup(
     author="Han Zhichao",
     author_email='superhin@126.com',
     description='Reuse pytest.ini to store env variables',
-    long_description='Reuse pytest.ini to store env variables',
+    long_description=read_file('README.md'),
+    long_description_content_type="text/markdown",
     classifiers=[
         'Framework :: Pytest',
         'Programming Language :: Python',
@@ -28,7 +36,7 @@ setup(
     packages=find_packages(include=['pytest_ini']),
     setup_requires=setup_requirements,
     url='https://github.com/hanzhichao/pytest-ini',
-    version='0.2',
+    version='0.3',
     zip_safe=True,
     install_requires=[
         'pytest',
